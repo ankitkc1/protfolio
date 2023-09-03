fc9a18a4101f917beada1acd99f02578711abdc8
Ankit K C <106390224+ankitkc1@users.noreply.github.com>
2023-09-03 05:56:23 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ankit K C",
+  title: "Front-End Web Developer & Researcher",
+  email: "ankitkc908@gmail.com",
+  gitHub: "ankitkc1",
+  instagram: "ankitkc1",
+  linkedIn: "ankitkc1",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
